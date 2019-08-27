a7ffdd6e9749302a59feeaacb8eaa0e2cde6fc6b
Manuel Ferriz <augusto.ferriz@gmail.com>
2019-08-27 16:57:45 -0700

    se agrego la funcionalidad Async await para obtener los datos de la llamada

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,8 +4,10 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term,
         per_page: 5
@@ -15,12 +17,15 @@ class App extends React.Component {
           "Client-ID 806a25804f6148e3ffef28ef232c6934b037ff26eab81f527623ca8234f3b6ff"
       }
     });
+
+    this.setState({ images: response.data.results });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <p>Found: {this.state.images.length} images</p>
       </div>
     );
   }
