6e84ee395217276191e941107dd9b025b4c3dade
Manuel Ferriz <augusto.ferriz@gmail.com>
2019-08-27 09:57:04 -0700

    se agrego la libreria axios para hacer peticiones a una API de imagenes

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit = term => {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+        per_page: 5
+      },
+      headers: {
+        Authorization:
+          "Client-ID 806a25804f6148e3ffef28ef232c6934b037ff26eab81f527623ca8234f3b6ff"
+      }
+    });
   };
 
   render() {
